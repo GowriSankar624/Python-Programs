e27a28b7d8652494c6209d01464bf34a7a7120cd
GowriSankar624 <77567327+GowriSankar624@users.noreply.github.com>
2021-02-10 13:37:18 +0530

    Update pacakge.py

--- python/pacakge.py
+++ python/pacakge.py
@@ -1,5 +1,5 @@
 import graphics.rectangle
-print("Rectangle")
+print("rectangle")
 l=int(input("enter the length:"))
 b=int(input("enter the breadth:"))
 print("Area: ",graphics.rectangle.area(l,b))
@@ -8,26 +8,26 @@ print("perimeter: ",graphics.rectangle.perimeter(l,b))
 
 from graphics.circle import area as a
 from graphics.cicrle import perimeter as p
-print("\nCircle")
+print("\ncircle")
 r=int(input("enter the radius: "))
-print("area:", a(r))
-print("perimeter:",p(r))
+print("area: ",a(r))
+print("perimeter: ",p(r))
 
       
       
 from graphics.dgraphics.cubiod import area as a
 from graphics.dgraphics.cubiod import perimeter as p
-print("\nCuboid")
+print("\ncuboid")
 l=int(input("enter the length:"))
 b=int(input("enter the breadth:"))
 h=int(input("enter the height:"))
-print("area:",a(l,b,h))
-print("perimeter:",p(1,b,h))
+print("area: ",a(l,b,h))
+print("perimeter: ",p(1,b,h))
 
 
 
 from graphics.dgraphics.sphere import*
-print("\nSphere")
+print("\nsphere")
 r=int(input("enter the radius of sphere:"))
-print("area:",area(r))
-print("circumference of sphere:",cirucmference(r))
+print("area: ",area(r))
+print("circumference of sphere:",circumference(r))
